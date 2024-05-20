eb886cbe5ade96de4c187a9348ce78e644fc45cb
DavidA346 <davidaispuro346@gmail.com>
2024-05-19 20:19:54 -0700

    Added signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Login from './pages/Login.jsx';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <Login />
-
+      <Signup />
     </div>
   );
 }
