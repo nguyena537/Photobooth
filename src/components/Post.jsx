47b87aca8224d4c49fd405bddd7621375ccb3e87
jahooba <josh.uapena20002@gmail.com>
2024-05-24 19:36:25 -0700

    Added components and basic styling for posts.

---
+++ src/components/Post.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Post.css';
+import defaultAvatar from '../assets/default avatar.png';
+import heartSVG from '../assets/heart interact.svg';
+import commentSVG from '../assets/comment interact.svg';
+import defaultPhoto from '../assets/default photo.png';
+
+const Post = () => {
+  return (
+    <div className='post'>
+      <div className='post-handle'>
+        <img className='post-avatar' src={defaultAvatar} alt='Avatar'/>
+        <p>Test Handle</p>
+      </div>
+
+      <div className='post-photo'>
+        <img className='post-photo-image' src={defaultPhoto} alt='Pic'/>
+      </div>
+
+      <div className='post-interact'>
+        <img className='post-interact-heart' src={heartSVG} alt='Heart Interact'/>
+        <img className='post-interact-comment' src={commentSVG} alt='Comment Interact'/>
+      </div>
+
+      <div className='post-text'>
+        <p>Test Caption</p>
+        <p>Test Comment</p>
+      </div>
+    </div>
+  )
+}
+
+export default Post
