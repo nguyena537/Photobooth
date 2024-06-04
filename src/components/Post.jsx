311e17f8ee3d6892180bb6f0c19e6c86c9c97e2b
jahooba <josh.uapena20002@gmail.com>
2024-06-04 11:35:12 -0700

    Staging posting functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Post.css';
 import defaultAvatar from '../assets/default avatar.png';
-import heartSVG from '../assets/heart interact.svg';
-import commentSVG from '../assets/comment interact.svg';
+import { FcLike } from "react-icons/fc";
+import { FaRegComment } from "react-icons/fa";
 import defaultPhoto from '../assets/default photo.png';
 
 const Post = (props) => {
-  const { username, userImage, postImage } = props;
+  const { post_id, username, userImage, postImage, likes, caption, comments } = props;
+  const [likeNum, setLikes] = useState(likes);
+  const [clicked, setClicked] = useState(false);
+
+  const handleLikes = async () => {
+    try {
+      const response = await fetch('/like', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: sessionStorage.getItem('token')
+        },
+        body: JSON.stringify({post_id})
+      });
+
+      if(!response.ok){
+        throw new Error('Failed to update like state');
+      }
+
+      const result = await response.json();
+
+      setClicked(!clicked);
+      setLikes(result.post.likes);
+    }
+    catch (error){
+      console.error('Error updating likes: ', error);
+    }
+  };
 
   return (
     <div className='post'>
       <div className='post-handle'>
-        <a href='http://localhost:3000/' alt='Change to user profile link'><img className='post-avatar' src={userImage ?? defaultAvatar} alt='Avatar'/></a>
+        <a href={`http://localhost:3000/profile/${username}`}><img className='post-avatar' src={userImage ?? defaultAvatar} alt='Avatar'/></a>
         <p>@{username}</p>
       </div>
 
       <div className='post-photo'>
-        <img className='post-photo-image' src={postImage} alt='Pic'/>
+        <img className='post-photo-image' src={postImage ?? defaultPhoto} alt='Pic'/>
       </div>
 
       <div className='post-interact'>
-        <img className='post-interact-heart' src={heartSVG} alt='Heart Interact'/>
-        <img className='post-interact-comment' src={commentSVG} alt='Comment Interact'/>
+        <FcLike className={`post-interact-heart ${clicked ? 'clicked' : ''}`} size={40} onClick={handleLikes}/>
+        <FaRegComment className='post-interact-comment' size={40}/>
       </div>
 
       <div className='post-text'>
-        <p>Test Caption</p>
-        <p>Test Comment</p>
+        <p>{likeNum} likes</p>
+        <p>{caption}</p>
+        <p>{comments}</p>
       </div>
     </div>
   )
--- src/pages/Postings.jsx
+++ src/pages/Postings.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Postings.css';
 import Post from '../components/Post';
 
 const Postings = () => {
+
+  const[posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('/post', {
+          headers: {
+            Authorization: sessionStorage.getItem('token')
+          }
+        });
+        const data = await response.json();
+        setPosts(data);
+      }
+      catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className='postings-row'>
-        <Post/>
-        <Post/>
-        <Post/>
+      {posts.map(post => (
+        <Post 
+          key={post.post_id} 
+          post_id={post.post_id} 
+          username={post.user_username}
+          userImage={post.user_image}
+          postImage={post.post_image}
+          likes={post.likes}
+          caption={post.description}
+          comments={post.comments}  
+        />
+      ))}
     </div>
   )
 }
 
-export default Postings
+export default Postings;
