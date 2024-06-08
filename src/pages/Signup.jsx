c8f66971e55b823589fd49e5d0091951424cfeb7
Andre Nguyen <nguyena537@gmail.com>
2024-06-07 21:26:25 -0700

    center login and signup pages

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -86,72 +86,75 @@ const Signup = () => {
             <img src={logo} alt="Photobooth Logo" className='logo'/>
         </div>
         
-        <div className="main-container">
-            <div className="top-container">
-                <h2 className='title'>Create an account</h2>
-                <p className="login-link">Already have an account? <span className='login-redirect' onClick={() => navigatePages('/')}>Log in</span></p>
-            </div>
-            
-            <div className="form-container">
-                <div className="forms" id='top-form'>
-                    <label htmlFor="profile-name" className='box-text'>What is your full name?</label> <br />
-                    <input type="text" name="name" id="name" className='form-boxes' placeholder='Full name' onChange={handleFormChange} required/>
+        <div className="center-div">
+            <div className="signup-main-container">
+                <div className="top-container">
+                    <h2 className='title'>Create an account</h2>
+                    <p className="login-link">Already have an account? <span className='login-redirect' onClick={() => navigatePages('/')}>Log in</span></p>
                 </div>
-
-                <div className="forms" id='top-form'>
-                    <label htmlFor="profile-name" className='box-text'>What should we call you?</label> <br />
-                    <input type="text" name="username" id="username" className='form-boxes' placeholder='Enter your profile name' onChange={handleFormChange} required/>
-                </div>
-
-                <div className="forms" id='middle-form'>
-                    <label htmlFor="email" className='box-text'>What's your email?</label> <br />
-                    <input type="email" name="email" id="email" className='form-boxes' placeholder='Enter your email address' onChange={handleFormChange} required/>
-                </div>
-
-                <div className="forms" id='bottom-form'>
-                        <div className="password-row">
-                            <label htmlFor="password" className='box-text'>Create a password</label>
-                            <button
-                                type="button"
-                                onClick={() => setPasswordVisibility(!passwordVisibility)}
-                                className="password-toggle-button"
-                            >
-                                {passwordVisibility ? (
-                                    <>
-                                        <img src={hideIcon} alt="Hide" className="hide-icon" />
-                                        Hide
-                                    </>
-                                ) : "Show"}
-                            </button>
-                        </div>
-                        <input
-                            type={passwordVisibility ? "text" : "password"}
-                            name="password"
-                            id="password"
-                            className='form-boxes'
-                            placeholder='Enter your password'
-                            onChange={handleFormChange}
-                            required
-                        />
+                
+                <div className="form-container">
+                    <div className="forms" id='top-form'>
+                        <label htmlFor="profile-name" className='box-text'>What is your full name?</label> <br />
+                        <input type="text" name="name" id="name" className='form-boxes' placeholder='Full name' onChange={handleFormChange} required/>
                     </div>
-                    <p className='password-requirements'></p>
-                    <button
-                        type='button'
-                        id='create-account-button'
-                        onClick={handleSignup}
-                        disabled={loading}
-                    >
-                        {loading ? "Loading..." : "Create an account"}
-                    </button>
-                    {signupFailed && <p>Signup failed</p>}
-                    {emailExists && <p>An account with this email already exists</p>}
-                </div>            
-            <p className='line-break'><span>OR</span></p>
 
-            <div className="bottom-container">
-                <GoogleButton className="google-button" onClick={auth} disabled={loading}/>
+                    <div className="forms" id='top-form'>
+                        <label htmlFor="profile-name" className='box-text'>What should we call you?</label> <br />
+                        <input type="text" name="username" id="username" className='form-boxes' placeholder='Enter your profile name' onChange={handleFormChange} required/>
+                    </div>
+
+                    <div className="forms" id='middle-form'>
+                        <label htmlFor="email" className='box-text'>What's your email?</label> <br />
+                        <input type="email" name="email" id="email" className='form-boxes' placeholder='Enter your email address' onChange={handleFormChange} required/>
+                    </div>
+
+                    <div className="forms" id='bottom-form'>
+                            <div className="password-row">
+                                <label htmlFor="password" className='box-text'>Create a password</label>
+                                <button
+                                    type="button"
+                                    onClick={() => setPasswordVisibility(!passwordVisibility)}
+                                    className="password-toggle-button"
+                                >
+                                    {passwordVisibility ? (
+                                        <>
+                                            <img src={hideIcon} alt="Hide" className="hide-icon" />
+                                            Hide
+                                        </>
+                                    ) : "Show"}
+                                </button>
+                            </div>
+                            <input
+                                type={passwordVisibility ? "text" : "password"}
+                                name="password"
+                                id="password"
+                                className='form-boxes'
+                                placeholder='Enter your password'
+                                onChange={handleFormChange}
+                                required
+                            />
+                        </div>
+                        <p className='password-requirements'></p>
+                        <button
+                            type='button'
+                            id='create-account-button'
+                            onClick={handleSignup}
+                            disabled={loading}
+                        >
+                            {loading ? "Loading..." : "Create an account"}
+                        </button>
+                        {signupFailed && <p>Signup failed</p>}
+                        {emailExists && <p>An account with this email already exists</p>}
+                    </div>            
+                <p className='line-break'><span>OR</span></p>
+
+                <div className="bottom-container">
+                    <GoogleButton className="google-button" onClick={auth} disabled={loading}/>
+                </div>
             </div>
         </div>
+        
     </div>
   )
 }
